3b8ac7b86ee60d5d6765ba4c3a4178475f02d7b8
michal-cesky <99603937+michal-cesky@users.noreply.github.com>
2022-10-10 18:39:52 +0200

    Add files via upload

---
+++ cv3/cv3/src/main/java/mds/cv3/Cv3Application.java
@@ -0,0 +1,12 @@
+package mds.cv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cv3Application.class, args);
+	}
+}
---
+++ cv3/cv3/src/test/java/mds/cv3/Cv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package mds.cv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
