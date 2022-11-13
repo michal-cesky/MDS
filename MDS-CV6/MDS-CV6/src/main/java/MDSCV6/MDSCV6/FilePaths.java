b4d3f358ced7a86da0df4549d63434e76b826e82
Michal Český <xcesky00@vutbr.cz>
2022-11-13 21:16:45 +0100

    comments

--- MDS-CV6/MDS-CV6/src/main/java/MDSCV6/MDSCV6/FilePaths.java
+++ MDS-CV6/MDS-CV6/src/main/java/MDSCV6/MDSCV6/FilePaths.java
@@ -4,7 +4,8 @@ import java.io.File;
 
 public class FilePaths {
 
-    private final static String BASE_PATH = "D:\\MDS\\files\\";
+    //cesty k souborum ve skole
+    /*private final static String BASE_PATH = "D:\\MDS\\files\\";
 
 
     public final static String MP4_FILE = BASE_PATH + "videos\\bbb_1080p.mp4";
@@ -16,6 +17,19 @@ public class FilePaths {
 
     public final static String MP4_DIRECTORY = BASE_PATH + "videos\\";
     public final static String IMAGES_DIRECTORY = BASE_PATH + "videos\\images\\";
+    public final static String SUFFIX = "mp4";*/
+
+    private final static String BASE_PATH = "C:\\Users\\Oem\\OneDrive - Vysoké učení technické v Brně\\škola\\VŠ\\3. ročník\\MDS\\files\\";
+
+    public final static File MP4_FILE = new File(BASE_PATH + "Videosoubory\\video.mp4");
+
+
+    public final static String HLS_PATH = BASE_PATH + "HLS\\";
+    public final static String DASH_PATH = BASE_PATH + "MPEG-DASH\\";
+
+
+    public final static String MP4_DIRECTORY = BASE_PATH + "Videosoubory\\";
+    public final static String IMAGES_DIRECTORY = BASE_PATH + "IMAGES";
     public final static String SUFFIX = "mp4";
 
 
